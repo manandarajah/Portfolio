f677c24c345a09d4663b316e3bcebd43e5620a13
Mugiesshan Anandarajah <manandarajah@users.noreply.github.com>
2018-09-05 11:41:36 -0400

    Update Database.java

--- My Utils/Database.java
+++ My Utils/Database.java
@@ -1,14 +1,13 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import org.apache.commons.dbcp.BasicDataSource;
 
 public abstract class Database implements AutoCloseable {
     
     private Connection CONN;
+    private ResultSet rs = null;
+    private boolean SecureConn = true;
     
-    protected void initConnection(String CONN_URL, String USERNAME, String PASSWORD) throws SQLException {
+    protected void initConnectionMySQL(String CONN_URL, String USERNAME, String PASSWORD) throws SQLException {
         BasicDataSource bds = new BasicDataSource();
         bds.setDriverClassName("com.mysql.jdbc.Driver");
         bds.setUrl(CONN_URL);
@@ -17,7 +16,28 @@ public abstract class Database implements AutoCloseable {
         CONN = bds.getConnection();
     }
     
-    public boolean executeStatement(String sql, Object[] list, ResultSet rs) throws SQLException {
+    protected void initConnectionPostgreSQL(String HOST, String DATA_BASE, String USERNAME, String PASSWORD) {
+        PGPoolingDataSource source = new PGPoolingDataSource();
+        source.setDataSourceName("A Data Source");
+        source.setServerName(HOST);
+        source.setDatabaseName(DATA_BASE);
+        source.setUser(USERNAME);
+        source.setPassword(PASSWORD);
+        source.setMaxConnections(10);
+        CONN = bds.getConnection();
+    }
+    
+    protected void initConnection(String CONN_URL, String USERNAME, String PASSWORD) {
+        try {
+            CONN = DriverManager.getConnection(CONN_URL, USERNAME, PASSWORD);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+            secureConn = false;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public boolean executeStatement(String sql, Object[] list) throws SQLException {
         
         try (PreparedStatement stmt = CONN.prepareStatement(sql)) {
             
@@ -36,13 +56,25 @@ public abstract class Database implements AutoCloseable {
                     || sql.contains("DELETE")
                     || sql.contains("update")
                     || sql.contains("UPDATE"))
-                return stmt.executeUpdate() == 1;
+                stmt.executeUpdate();
+                return true;
             else if (sql.contains("select") || sql.contains("SELECT")) {
-                rs = stmt.executeQuery();
+                if (secureConn) {
+                    rs = stmt.executeQuery();
+                }
+                
+                else {
+                    Statement stmt2 = CONN.createStatement();
+                    rs = stmt2.executeQuery(sql);
+                }
                 return true;
             }
         }
         return false;
+    }
+    
+    public ResultSet getResultSet() {
+        return this.rs;   
     }
 
     @Override
